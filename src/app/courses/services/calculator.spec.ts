e0dcdad45409905005f0b78b07b77846a6d86086
lapeko <lapeko88@gmail.com>
2023-11-04 12:23:21 +0100

    use correct injections in tests

--- src/app/courses/services/calculator.spec.ts
+++ src/app/courses/services/calculator.spec.ts
@@ -1,26 +1,37 @@
+import {TestBed} from "@angular/core/testing";
+
 import {CalculatorService} from "./calculator.service";
 import {LoggerService} from "./logger.service";
 
 describe("Calculator service", () => {
   let calculatorService: CalculatorService;
-  let loggerService: LoggerService;
+  let loggerSpyService: jasmine.SpyObj<LoggerService>;
 
   beforeEach(() => {
-    loggerService = jasmine.createSpyObj("LoggerService", ["log"])
-    calculatorService = new CalculatorService(loggerService);
+    loggerSpyService = jasmine.createSpyObj("LoggerService", ["log"])
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide :LoggerService, useValue: loggerSpyService},
+      ]
+    });
+
+    calculatorService = TestBed.inject(CalculatorService);
+    loggerSpyService = TestBed.inject(LoggerService) as jasmine.SpyObj<LoggerService>;
   });
 
   it("should add two numbers", () => {
     const res = calculatorService.add(2, 3);
 
     expect(res).toBe(5);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpyService.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
     const res = calculatorService.subtract(2, 3);
 
     expect(res).toBe(-1);
-    expect(loggerService.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpyService.log).toHaveBeenCalledTimes(1);
   });
 });
