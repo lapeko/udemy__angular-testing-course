6cff40326526e4bf30cdf4b01c69d6b09a90b8c9
lapeko <lapeko88@gmail.com>
2023-11-04 12:02:05 +0100

    mock dependency

--- src/app/courses/services/calculator.spec.ts
+++ src/app/courses/services/calculator.spec.ts
@@ -3,19 +3,24 @@ import {LoggerService} from "./logger.service";
 
 describe("Calculator service", () => {
   let calculatorService: CalculatorService;
+  let loggerService: LoggerService;
+
   beforeEach(() => {
-    calculatorService = new CalculatorService(new LoggerService());
+    loggerService = jasmine.createSpyObj("LoggerService", ["log"])
+    calculatorService = new CalculatorService(loggerService);
   });
 
   it("should add two numbers", () => {
     const res = calculatorService.add(2, 3);
 
     expect(res).toBe(5);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
     const res = calculatorService.subtract(2, 3);
 
     expect(res).toBe(-1);
+    expect(loggerService.log).toHaveBeenCalledTimes(1);
   });
 });
