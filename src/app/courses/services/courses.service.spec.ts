896ee02faef4030b16a405fe3cb07d67a76ca1b0
lapeko <lapeko88@gmail.com>
2023-11-04 14:46:12 +0100

    use httpClientModule, add findCourseBeId testCase

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,38 +1,60 @@
 import {TestBed} from "@angular/core/testing";
-import {HttpClient} from "@angular/common/http";
-import {of} from "rxjs";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 import {CoursesService} from "./courses.service";
 import {Course} from "../model/course";
+import {COURSES} from "../../../../server/db-data";
 
 describe("CoursesService", () => {
+  const httpReturnCourses = Object.values(COURSES) as Course[];
   let coursesService: CoursesService;
-  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    const httpSpy = jasmine.createSpyObj("HttpClient", ["get", "put"]);
-
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [
         CoursesService,
-        {provide: HttpClient, useValue: httpSpy},
       ]
     })
 
     coursesService = TestBed.inject(CoursesService);
-    httpClientSpy = TestBed.inject(HttpClient) as jasmine.SpyObj<HttpClient>;
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it("should return expected courses (HttpClient called once)", (done) => {
-    const expectedCourses: Course[] = [{} as Course];
-    httpClientSpy.get.and.returnValue(of({payload: expectedCourses}));
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it("findAllCourses should return expected courses (HttpClient called once)", (done) => {
+    const expectedCourses: Course[] = Object.values(COURSES);
 
     const res = coursesService.findAllCourses();
 
     res.subscribe(courses => {
-      console.log(courses);
       expect(courses).toEqual(expectedCourses);
       done();
     });
+
+    const req = httpTestingController.expectOne('/api/courses');
+    req.flush({payload: httpReturnCourses});
+
+    expect(req.request.method).toBe("GET");
+  });
+
+  it("findCourseById should return expected course (HttpClient called once)", (done) => {
+    const expectedCourse = COURSES[12] as Course;
+
+    const res = coursesService.findCourseById(12);
+
+    res.subscribe(course => {
+      expect(course).toEqual(expectedCourse);
+      done();
+    });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+    req.flush(expectedCourse);
+
+    expect(req.request.method).toBe("GET");
   });
 });
