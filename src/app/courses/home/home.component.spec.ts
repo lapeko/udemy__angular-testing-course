ddeade708083688519cb47929b5ca244f920564b
lapeko <lapeko88@gmail.com>
2023-11-07 23:12:15 +0100

    test home-component without async logic

---
+++ src/app/courses/home/home.component.spec.ts
@@ -0,0 +1,86 @@
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {DebugElement} from '@angular/core';
+import {of} from "rxjs";
+import {By} from "@angular/platform-browser";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+
+import {HomeComponent} from './home.component';
+import {CoursesModule} from "../courses.module";
+import {CoursesService} from "../services/courses.service";
+import {setupCourses} from "../common/setup-test-data";
+import {click} from "../common/test-utils";
+
+describe('HomeComponent', () => {
+  const allCourses = setupCourses();
+  const beginnerCourses = allCourses.filter(({category}) => category === "BEGINNER");
+  const advancedCourses = allCourses.filter(({category}) => category === "ADVANCED");
+
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let el: DebugElement;
+  let coursesService: jasmine.SpyObj<CoursesService>;
+
+  beforeEach(waitForAsync(() => {
+    const CourseServiceMock = jasmine.createSpyObj("CoursesService", ["findAllCourses"])
+    TestBed.configureTestingModule({
+      imports: [
+        CoursesModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {provide: CoursesService, useValue: CourseServiceMock}
+      ],
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    el = fixture.debugElement;
+    coursesService = TestBed.inject(CoursesService) as jasmine.SpyObj<CoursesService>;
+  });
+
+  it("should create the component", () => {
+    expect(component).toBeTruthy();
+  });
+
+
+  it("should display only beginner courses", () => {
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(1);
+  });
+
+
+  it("should display only advanced courses", () => {
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(1);
+  });
+
+
+  it("should display both tabs", () => {
+    coursesService.findAllCourses.and.returnValue(of(allCourses));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    expect(tabs.length).toBe(2);
+  });
+
+
+  fit("should display advanced courses when tab clicked", () => {
+    coursesService.findAllCourses.and.returnValue(of(allCourses));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mdc-tab"));
+    click(tabs[1]);
+    fixture.detectChanges();
+
+    const titles = el.queryAll(By.css("mat-card-title")).map(m => m.nativeElement.innerText);
+    expect(1).toBe(1);
+  });
+});
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -1,48 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {CoursesService} from "../services/courses.service";
+import {Component, inject, OnInit} from '@angular/core';
 import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
+
+import {Course} from "../model/course";
+import {CoursesService} from "../services/courses.service";
 import {sortCoursesBySeqNo} from './sort-course-by-seq';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private readonly coursesService = inject(CoursesService);
 
-    beginnerCourses$: Observable<Course[]>;
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
 
-    advancedCourses$: Observable<Course[]>;
+  ngOnInit() {
+    this.reloadCourses();
+  }
 
-    constructor(private coursesService: CoursesService) {
+  reloadCourses() {
+    const courses$ = this.coursesService.findAllCourses();
+    this.beginnerCourses$ = this.filterByCategory(courses$, 'BEGINNER');
+    this.advancedCourses$ = this.filterByCategory(courses$, 'ADVANCED');
+  }
 
-    }
-
-    ngOnInit() {
-
-      this.reloadCourses();
-
-    }
-
-
-    reloadCourses() {
-
-      const courses$ = this.coursesService.findAllCourses();
-
-      this.beginnerCourses$ = this.filterByCategory(courses$, 'BEGINNER');
-
-      this.advancedCourses$ = this.filterByCategory(courses$, 'ADVANCED');
-
-    }
-
-    filterByCategory(courses$: Observable<Course[]>, category:string) {
-      return courses$.pipe(
-        map(courses => courses.filter(course => course.category === category).sort(sortCoursesBySeqNo) )
-      );
-    }
-
+  filterByCategory(courses$: Observable<Course[]>, category: string) {
+    return courses$.pipe(
+      map(courses => courses.filter(course => course.category === category).sort(sortCoursesBySeqNo))
+    );
+  }
 }
-
-
